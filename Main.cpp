99f1ebe3b32032f9e7d3e6b7b32a6ab4242ca3c9
Skyler R Stevens <Skyler.R.Stevens@gmail.com>
2026-07-09 19:58:42 -0600

    Initial CMake project setup

---
+++ Main.cpp
@@ -0,0 +1,14 @@
+﻿// Snakes Vs Centipedes.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+#include "Game.h"
+
+//
+// using namespace std;
+
+int main()
+{
+	std::cout << "Snakes Vs Centipedes" << std::endl;
+	return 0;
+}
